668071e2723169155ec69fa607ca00c275f52421
malucien <124107514+malucien@users.noreply.github.com>
2023-03-14 21:07:43 -0500

    First commit

--- 02-mapping-messages/sweetMessages.js
+++ 02-mapping-messages/sweetMessages.js
@@ -4,4 +4,13 @@ const messages = [
     'would you like to get a bite later'
 ];
 
-const sweetMessages = `${messages[0]}, sweetie?`;
+//const sweetMessages = `${messages[0]}, sweetie?`;
+
+const sweetMessages =[];
+
+for (let i = 0; i < message.length; i++) {
+    const newMessage = `${messages[i]}, sweetie?`;
+    sweetMessages.push(newMessage);
+}
+
+console.log(sweetMessages)
